7f810cd3fa5680b23c3c3089981a7f99e75f2f94
Nuno Jesus <ncarvalh@student.42porto.com>
2023-06-29 16:11:36 +0100

    Code: created commons structs.

--- includes/miniRT.h
+++ includes/miniRT.h
@@ -6,7 +6,7 @@
 /*   By: ncarvalh <ncarvalh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/29 13:01:11 by ncarvalh          #+#    #+#             */
-/*   Updated: 2023/06/29 15:09:17 by ncarvalh         ###   ########.fr       */
+/*   Updated: 2023/06/29 16:08:05 by ncarvalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,73 @@
 
 # include "libnc.h"
 # include "vector.h"
+
+# define PI 3.1415926535
+# define RADIANS(deg) deg * PI / 180.0
+# define DEGREES(rad) rad * 180.0 / PI
+
+typedef struct s_color
+{
+	int	r;
+	int	g;
+	int	b;
+} t_color;
+
+typedef struct s_lightsource
+{
+	t_vec3	origin;
+	float	brightness;
+	t_color color;
+} t_lightsource;
+
+typedef struct s_light
+{	
+	float	ratio;
+	t_color color;
+} t_light;
+
+typedef struct s_camera
+{
+	t_vec3	origin;
+	t_vec3	normal;
+	float	fov;
+} t_camera;
+
+typedef struct s_sphere
+{
+	t_vec3	origin;
+	float	radius;
+	t_color	color;
+} t_sphere;
+
+typedef struct s_plane
+{
+	t_vec3	point;
+	t_vec3	normal;
+	t_color	color;
+} t_plane;
+
+typedef struct s_cylinder
+{
+	t_vec3	origin;
+	t_vec3	normal;
+	float radius;
+	float height;
+	t_color	color;
+} t_cylinder;
+
+typedef struct s_root
+{
+	t_camera		camera;
+	t_light			ambient;
+	t_lightsource	*source;
+	t_sphere		*spheres;
+	t_plane			*planes;
+	t_cylinder		*cylinder;
+	int				num_sources;
+	int				num_spheres;
+	int				num_planes;
+	int				num_cylinder;
+} t_root;
 
 # endif
