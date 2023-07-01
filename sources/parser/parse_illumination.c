667ba6b7fdfe2dbeb4682225161ce1959efa1734
Mario <mariojunior200218@gmail.com>
2023-07-01 16:52:40 +0100

    finished parser code for ilumination, need to test it

--- sources/parser/parse_illumination.c
+++ sources/parser/parse_illumination.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parse_illumination.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: maricard <maricard@student.42.fr>          +#+  +:+       +#+        */
+/*   By: maricard <maricard@student.porto.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/30 15:12:35 by ncarvalh          #+#    #+#             */
-/*   Updated: 2023/06/30 16:43:34 by maricard         ###   ########.fr       */
+/*   Created: 2023/07/01 15:12:25 by maricard          #+#    #+#             */
+/*   Updated: 2023/07/01 16:50:14 by maricard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,34 +15,55 @@
 void	parse_ambient_light(t_root *root, char **tokens)
 {
 	char 	**data;
-
-	// ! check number of args
+	int 	i;
+	
+	i = -1;
+	if (nc_matrix_size(tokens) != 4)
+		message(root, "Wrong number of arguments for ambient light");
 	if (!parse_syntax(tokens, "001"))
 		message(root, "Wrong syntax for ambient light");
-	data = nc_split(tokens, ',');
-	root->ambient.ratio = nc_atof(data[0]);
-	root->ambient.color.r = data[1];
-	root->ambient.color.g = data[2];
-	root->ambient.color.b = data[3];
+	while (tokens[++i])
+		data = nc_split(tokens[i], ',');
+	root->ambient.ratio = ft_atof(data[0]);
+	root->ambient.color.r = ft_atof(data[1]);
+	root->ambient.color.g =  ft_atof(data[2]);
+	root->ambient.color.b = ft_atof(data[3]);
+	printf("nc_matrix_size(tokens) = %zu\n", nc_matrix_size(tokens));
+	printf("root->ambient.ratio = %f\n", root->ambient.ratio);
+	printf("root->ambient.color.r = %d\n", root->ambient.color.r);
+	printf("root->ambient.color.g = %d\n", root->ambient.color.g);
+	printf("root->ambient.color.b = %d\n", root->ambient.color.b);
 	(void) root;
 	(void) tokens;
 }
 
 void	parse_camera(t_root *root, char **tokens)
 {
-	char **data;
+	char	 **data;
+	int 	i;
 	
-	// ! check number of args
+	i = -1;
+	if (nc_matrix_size(tokens) != 7)
+		message(root, "Wrong number of arguments for camera");
 	if (!parse_syntax(tokens, "0110"))
 		message(root, "Wrong syntax for camera");
-	data = nc_split(tokens, ',');
-	root->camera.origin.x = nc_atof(data[0]);
-	root->camera.origin.y = nc_atof(data[1]);
-	root->camera.origin.z = nc_atof(data[2]);
-	root->camera.normal.x = nc_atof(data[3]);
-	root->camera.normal.y = nc_atof(data[4]);
-	root->camera.normal.z = nc_atof(data[5]);
-	root->camera.fov = nc_atof(data[6]);
+	while (tokens[++i])
+		data = nc_split(tokens[i], ',');
+	root->camera.origin.x = ft_atof(data[0]);
+	root->camera.origin.y = ft_atof(data[1]);
+	root->camera.origin.z = ft_atof(data[2]);
+	root->camera.normal.x = ft_atof(data[3]);
+	root->camera.normal.y = ft_atof(data[4]);
+	root->camera.normal.z = ft_atof(data[5]);
+	root->camera.fov = ft_atof(data[6]);
+	printf("nc_matrix_size(tokens) = %zu\n", nc_matrix_size(tokens));
+	printf("root->camera.origin.x = %f\n", root->camera.origin.x);
+	printf("root->camera.origin.y = %f\n", root->camera.origin.y);
+	printf("root->camera.origin.z = %f\n", root->camera.origin.z);
+	printf("root->camera.normal.x = %f\n", root->camera.normal.x);
+	printf("root->camera.normal.y = %f\n", root->camera.normal.y);
+	printf("root->camera.normal.z = %f\n", root->camera.normal.z);
+	printf("root->camera.fov = %f\n", root->camera.fov);
 	(void) root;
 	(void) tokens;
 }
@@ -50,20 +71,32 @@ void	parse_camera(t_root *root, char **tokens)
 void	parse_light_source(t_root *root, char **tokens)
 {
 	char 	**data;
+	int 	i;
 	int 	k;
 	
-	// ! check number of args
+	i = -1;
+	if (nc_matrix_size(tokens) != 7)
+		message(root, "Wrong number of arguments for light source");
 	if (!parse_syntax(tokens, "0101"))
 		message(root, "Wrong syntax for light source");
-	data = nc_split(tokens, ',');
+	while (tokens[++i])
+		data = nc_split(tokens[i], ',');
 	k = root->num_sources;
-	root->source[k].origin.x = nc_atof(data[0]);
-	root->source[k].origin.y = nc_atof(data[1]);
-	root->source[k].origin.z = nc_atof(data[2]);
-	root->source[k].brightness = nc_atof(data[3]);
-	root->source[k].color.r = nc_atof(data[4]);
-	root->source[k].color.g = nc_atof(data[5]);
-	root->source[k].color.b = nc_atof(data[6]);
+	root->source[k].origin.x = ft_atof(data[0]);
+	root->source[k].origin.y = ft_atof(data[1]);
+	root->source[k].origin.z = ft_atof(data[2]);
+	root->source[k].brightness = ft_atof(data[3]);
+	root->source[k].color.r = ft_atof(data[4]);
+	root->source[k].color.g = ft_atof(data[5]);
+	root->source[k].color.b = ft_atof(data[6]);
+	printf("nc_matrix_size(tokens) = %zu\n", nc_matrix_size(tokens));
+	printf("root->source[k].origin.x = %f\n", root->source[k].origin.x);
+	printf("root->source[k].origin.y = %f\n", root->source[k].origin.y);
+	printf("root->source[k].origin.z = %f\n", root->source[k].origin.z);
+	printf("root->source[k].brightness = %f\n", root->source[k].brightness);
+	printf("root->source[k].color.r = %d\n", root->source[k].color.r);
+	printf("root->source[k].color.g = %d\n", root->source[k].color.g);
+	printf("root->source[k].color.b = %d\n", root->source[k].color.b);
 	root->num_sources++;
 	(void) root;
 	(void) tokens;
