a6c730a8b56c70a87ca6040122a2ad5e9c6fe7f6
Nuno Jesus <up201905477@edu.fe.up.pt>
2023-08-14 18:25:44 +0100

    Docs: documented structs.h

--- includes/structs.h
+++ includes/structs.h
@@ -6,13 +6,16 @@
 /*   By: crypto <crypto@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/29 16:20:18 by ncarvalh          #+#    #+#             */
-/*   Updated: 2023/08/13 21:03:54 by crypto           ###   ########.fr       */
+/*   Updated: 2023/08/14 18:25:29 by crypto           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef STRUCTS_H
 # define STRUCTS_H
 
+/**
+ * @brief Describes what shape you're dealing with
+ */
 typedef enum s_shape_type
 {
 	PLANE,
@@ -20,6 +23,12 @@ typedef enum s_shape_type
 	CYLINDER,
 }	t_shape_type;
 
+/**
+ * @brief Represents a tridimensional vector
+ * @param x The x component
+ * @param y The y component
+ * @param z The z component
+ */
 typedef struct s_vec3
 {
 	double	x;
@@ -27,6 +36,13 @@ typedef struct s_vec3
 	double	z;
 }	t_vec3;
 
+/**
+ * @brief Represents a color by its components
+ * @param t The transparency component
+ * @param r The red component
+ * @param g The green component
+ * @param b The blue component
+ */
 typedef struct s_color
 {
 	int	t;
@@ -35,6 +51,16 @@ typedef struct s_color
 	int	b;
 }	t_color;
 
+/**
+ * @brief Contains the necessary fields to handle both second degree
+ * and linear equations.
+ * 
+ * @param a The a value
+ * @param b The b value
+ * @param c The c value
+ * @param t1 The first root
+ * @param t2 The second root
+ */
 typedef struct s_equation
 {
 	double	a;
@@ -44,12 +70,27 @@ typedef struct s_equation
 	double	t2;
 }	t_equation;
 
+/**
+ * @brief A scene's ray
+ * 
+ * @param origin The origin of the ray
+ * @param direction The direction of the ray
+ */
 typedef struct s_ray
 {
 	t_vec3	origin;
 	t_vec3	direction;
 }	t_ray;
 
+/**
+ * @brief Used to represent both the ambient light and a light source.
+ * 
+ * @param center The center of the light source
+ * @param ratio The ratio/brightness of the light
+ * @param color The light's color
+ * 
+ * @note The center field is not used in ambient lighting
+ */
 typedef struct s_light
 {
 	t_vec3	center;
@@ -57,6 +98,13 @@ typedef struct s_light
 	t_color	color;
 }	t_light;
 
+/**
+ * @brief Describes how the camera is positioned in the scene
+ * 
+ * @param center The center of the camera
+ * @param normal The direction of the camera
+ * @param fov The camera's Field Of View 
+ */
 typedef struct s_camera
 {
 	t_vec3	center;
@@ -64,6 +112,13 @@ typedef struct s_camera
 	double	fov;
 }	t_camera;
 
+/**
+ * @brief Describes a sphere
+ * 
+ * @param center The center of the sphere
+ * @param radius The radius of the sphere
+ * @param color The color of the sphere
+ */
 typedef struct s_sphere
 {
 	t_vec3	center;
@@ -71,6 +126,13 @@ typedef struct s_sphere
 	t_color	color;
 }	t_sphere;
 
+/**
+ * @brief Describes an infinite plane
+ * 
+ * @param center A point in the plane (not really its center)
+ * @param normal The direction the plane is pointing at
+ * @param color The color of the plane
+ */
 typedef struct s_plane
 {
 	t_vec3	center;
@@ -78,6 +140,22 @@ typedef struct s_plane
 	t_color	color;
 }	t_plane;
 
+/**
+ * @brief Describes a cylinder
+ * 
+ * @param center The center of a cylinder
+ * @param cap1 The cap poin of the plane calculated using:
+ * 
+ * 		cap1 = center - height/2 * Normal Vector
+ * 
+ * @param cap2 The cap poin of the plane calculated using:
+ * 
+ * 		cap2 = center + height/2 * Normal Vector
+ * @param normal The direction of the cylinder
+ * @param radius The radius of the cylinder
+ * @param height The height of the cylinder
+ * @param color The color of the cylinder
+ */
 typedef struct s_cylinder
 {
 	t_vec3	center;
@@ -89,6 +167,15 @@ typedef struct s_cylinder
 	t_color	color;
 }	t_cylinder;
 
+/**
+ * @brief Contains the data of only ONE of the shapes at a time.
+ * This means that if the cylinder is used, reading from the plane or 
+ * sphere will give you random values.
+ * 
+ * @param sp The t_sphere struct
+ * @param pl The t_plane struct
+ * @param cy The t_cylinder struct
+ */
 typedef union u_data
 {
 	t_sphere	sp;
@@ -96,6 +183,13 @@ typedef union u_data
 	t_cylinder	cy;
 }	t_data;
 
+/**
+ * @brief Describes a shape
+ * 
+ * @param data The union containing the shape data
+ * @param type The type of shape
+ * @param id A number between 0 and the number of shapes in the scene
+ */
 typedef struct s_shape
 {
 	t_data			data;
@@ -103,6 +197,19 @@ typedef struct s_shape
 	int				id;
 }	t_shape;
 
+/**
+ * @brief Contains mlx related variables
+ * 
+ * @param mlx The mlx pointer
+ * @param win The mlx window pointer
+ * @param img The mlx image frame pointer
+ * @param addr The address of the first byte of the mlx image
+ * @param height The image's height
+ * @param width The image's width
+ * @param bpp The bytes per pixel in the image pointer
+ * @param line_length (not used)
+ * @param endian (not used)
+ */
 typedef struct s_graphics
 {
 	void	*mlx;
@@ -116,6 +223,18 @@ typedef struct s_graphics
 	int		endian;
 }	t_graphics;
 
+/**
+ * @brief Describes an intersection point in the scene between 
+ * a ray and a shape.
+ * 
+ * @param shape The shape that was hit
+ * @param ray The ray that hit the shape
+ * @param point The intersection point
+ * @param normal The normal of the shape in the intersection point
+ * @param color The color of the hit shape
+ * @param a Used to know where the ray hit in a cylinder's axis
+ * @param t The distance between the hit the ray's origin
+ */
 typedef struct s_hit
 {
 	t_shape	*shape;
@@ -127,6 +246,21 @@ typedef struct s_hit
 	double	t;
 }	t_hit;
 
+/**
+ * @brief The primary struct, contains everything needed to describe a scene
+ * 
+ * @param camera The scene's camera
+ * @param ambient The scene's ambient light
+ * @param lights The scenes' light sources vector
+ * @param shapes The scene's shapes vector
+ * @param counters The number of A, C and L entities found when parsing
+ * @param disp The mlx variables
+ * @param up The viewport up vector
+ * @param right The viewport right vector
+ * @param map The map read from the given file
+ * @param hview The height of the viewport 
+ * @param wview The width of the viewport 
+ */
 typedef struct s_world
 {
 	t_camera	camera;
