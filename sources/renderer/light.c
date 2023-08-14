0ccdadab33143d1765ac20c2917674964cb1cc59
Nuno Jesus <ncarvalh@student.42porto.com>
2023-08-14 20:17:12 +0100

    Code: added dependency generation to Makefile.

--- sources/renderer/light.c
+++ sources/renderer/light.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   light.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: crypto <crypto@student.42.fr>              +#+  +:+       +#+        */
+/*   By: ncarvalh <ncarvalh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/03 15:23:25 by crypto            #+#    #+#             */
-/*   Updated: 2023/08/14 17:05:10 by crypto           ###   ########.fr       */
+/*   Updated: 2023/08/14 20:16:52 by ncarvalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,30 +59,11 @@ void	illuminate(t_world *world, t_hit *closest)
 	closest->color = color;
 }
 
-/* 
-	Ambient illumination (IA) is given by:
-
-	IA = k * shape_color
-
-	k - ambient illumination coefficient
-	shape_color - color of the shape
-	
-*/
 t_color	ambient(t_color	color, double ratio)
 {
 	return (color_mult(color, ratio));
 }
 
-/*
-	Given a ray L that has an angle A with the normal of a shape,
-	the diffuse illumination (ID) is given by:
-
-	ID = K * shape_color * cos(A)
-	K - diffuse coefficient (depends on the material)
-	shape_color - ineherent color of the shape
-	cos(A) - cossine of the angle between N and L
-	
-*/
 t_color	diffuse(t_light *bulb, t_hit *inter, double k)
 {
 	t_vec3	light_dir;
