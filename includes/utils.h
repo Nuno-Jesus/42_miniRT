7536cb1a0e365f678c37a5487c2958b289f85556
Mario <mariojunior200218@gmail.com>
2023-08-14 18:08:46 +0100

    documentation: added documentation for entities.h and utils.h files

--- includes/utils.h
+++ includes/utils.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   utils.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: crypto <crypto@student.42.fr>              +#+  +:+       +#+        */
+/*   By: maricard <maricard@student.porto.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/15 18:04:41 by ncarvalh          #+#    #+#             */
-/*   Updated: 2023/08/10 15:50:16 by crypto           ###   ########.fr       */
+/*   Updated: 2023/08/14 18:07:11 by maricard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,33 @@
 
 # include "miniRT.h"
 
+/**
+ * @brief funtion that prints the given error message as parameter and exits
+ * the program destroying the world struct (freeing all the memory 
+ * previously allocated)
+ * 
+ * @param world t_world struct with all the information of the world 
+ * @param text text to be printed
+ */
 void	message(t_world *world, char *text);
 
+/**
+ * @brief this function checks how many intersections the ray has with the
+ * object.
+ * 
+ * @param eq t_equation struct with the equation information
+ * @return number of intersections, -1 if there are no intersections
+ */
 double	determinant(t_equation *eq);
 
+/**
+ * @brief this function will solve the quadratic equation, store the results
+ * inside the t_equation struct (t1 and t2 variables) and return the number 
+ * of intersections the ray has with the object.
+ * 
+ * @param eq t_equation struct with the equation information
+ * @return number of intersections, -1 if there are no intersections
+ */
 double	solve(t_equation *eq);
 
 #endif
